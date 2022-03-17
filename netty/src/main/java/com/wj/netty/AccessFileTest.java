5105d4ed31eaeda993b698d73da70cf5414ccc08
wujiang <786299245@qq.com>
2022-03-17 14:53:04 +0800

    try-with-resource

--- netty/src/main/java/com/wj/netty/AccessFileTest.java
+++ netty/src/main/java/com/wj/netty/AccessFileTest.java
@@ -8,25 +8,22 @@ import java.nio.channels.FileChannel;
 
 public class AccessFileTest {
 
-
     /**
      * 顺序写
      */
     public static long fileWrite(String filePath, String content, int index) {
         File file = new File(filePath);
-        RandomAccessFile randomAccessTargetFile;
         //  操作系统提供的一个内存映射的机制的类
         MappedByteBuffer map;
-        try {
-            randomAccessTargetFile = new RandomAccessFile(file, "rw");
-            FileChannel targetFileChannel = randomAccessTargetFile.getChannel();
-            map = targetFileChannel.map(FileChannel.MapMode.READ_WRITE, 0, (long) 1024 * 1024 * 1024);
+        try (RandomAccessFile randomAccessTargetFile = new RandomAccessFile(file, "rw")) {
+
+            FileChannel channel = randomAccessTargetFile.getChannel();
+            map = channel.map(FileChannel.MapMode.READ_WRITE, 0, (long) 1024 * 1024 * 1024);
             map.position(index);
             map.put(content.getBytes());
             return map.position();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
         }
         return 0L;
     }
@@ -36,11 +33,9 @@ public class AccessFileTest {
      */
     public static String fileRead(String filePath, long index) {
         File file = new File(filePath);
-        RandomAccessFile randomAccessTargetFile;
         //  操作系统提供的一个内存映射的机制的类
         MappedByteBuffer map;
-        try {
-            randomAccessTargetFile = new RandomAccessFile(file, "rw");
+        try (RandomAccessFile randomAccessTargetFile = new RandomAccessFile(file, "rw")) {
             FileChannel targetFileChannel = randomAccessTargetFile.getChannel();
             map = targetFileChannel.map(FileChannel.MapMode.READ_WRITE, 0, index);
             byte[] byteArr = new byte[10 * 1024];
